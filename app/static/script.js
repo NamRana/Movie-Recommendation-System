1c58c806b1ea20cee8737e4a6aed32b7a1eb9048
Naman <sanjeevnamanrana@gmail.com>
2022-12-07 14:09:10 +0530

    Movie-Recommendation-System

---
+++ app/static/script.js
@@ -0,0 +1,39 @@
+function show_movies() {
+	$.ajax({
+		url: "/get_recommend",
+		type: "POST",
+		dataType: "json",
+		success: function (data) {
+			console.log("Ajax Success");
+			console.log(data);
+			$(dynamic_movies).replaceWith(data);
+		},
+	});
+}
+
+$(function () {
+	$("#clear-butt").click(function (e) {
+		e.preventDefault();
+		$.getJSON("/clear_movies", function (data) {
+			console.log(data);
+		});
+
+		$(".reset-alert").append(
+			`
+			<div class="alert alert-info alert-dismissible fade show" role="alert"
+			style="margin-top: 10px;">
+				<strong> Successfully Reset Movie Reviews </strong>
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>
+			`
+		);
+
+		$(".reset-alert").fadeTo(2000, 500).slideUp(500, function(){
+			$(".reset-alert").alert('close');
+		});
+	});
+});
+
+$(".alert-dismissible").fadeTo(2000, 500).slideUp(500, function(){
+    $(".alert-dismissible").alert('close');
+});
